2b3540071144119f91e3d242fd197709e08c22c1
Alek Howland <alekhowland@Aleks-MacBook-Pro.local>
2022-03-29 16:36:42 -0600

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Hooks } from "./Hooks";
+import { Formulario } from "./hooks/effect/Formulario";
+import { Mensaje } from "./hooks/effect/Mensaje";
+import { Contador } from "./hooks/state/Contador";
+import { ContadorConCustomHook } from "./hooks/state/ContadorConCustomHook";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Mensaje />, document.getElementById("root"));
