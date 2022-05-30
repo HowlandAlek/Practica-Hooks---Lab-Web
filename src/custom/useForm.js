89a57faa3d13ffc145beac5c70bfea122ede2d51
HowlandAlek <howlandalek@gmail.com>
2022-05-30 10:00:31 -0500

    Save files for backup

--- src/custom/useForm.js
+++ src/custom/useForm.js
@@ -1,20 +1,20 @@
 import { useState } from "react";
-
 export const useForm = (initialState = {}) => {
-    // Crear el estado inicial
-    const [formState, setFormState] = useState(initialState);
-
+    //Creamos el estado inicial con el hook useState.
+    const [formValues, setFormValues] = useState(initialState);
+    //Funcion que podemos utilizar para resetear los valores del formulario.
+    const reset = () => {
+        setFormValues(initialState);
+    };
     //Esta función se va invocar cada vez que exista un cambio en alguno de los input text del formulario.
     const handleInputChange = (e) => {
-        //console.log(e.target.name);
-        //console.log(e.target.value);
-        //Actualizamos el estado del formulario.
-        setFormState({
-            ...formState,
+        console.log(e.target.name);
+        console.log(e.target.value); //Actualizamos el estado del formulario.
+        setFormValues({
+            ...formValues,
             [e.target.name]: e.target.value,
         });
     };
-
-    //Regresamos un objeto que va tener un arreglo con el estado actual y la referencia a la funcion handleInputChange
-    return [formState, handleInputChange];
+    //El custom hook va regresar formValues, la función handleInputChange, y la función reset
+    return [formValues, handleInputChange, reset];
 };
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom";
 import { Hooks } from "./Hooks";
 import { Formulario } from "./hooks/effect/Formulario";
 import { Mensaje } from "./hooks/effect/Mensaje";
+import { HookCallback } from "./hooks/memo/HookCallBack";
 import { HookUseMemo } from "./hooks/memo/HookUseMemo";
 import { Memoriza } from "./hooks/memo/Memoriza";
 import { MultipleCustomHooks } from "./hooks/MultipleCustomHooks";
+import { TodoApp } from "./hooks/reducer/TodoApp";
 import { FocusScreen } from "./hooks/ref/FocusScreen";
 import { UseRefMultipleCustomHooks } from "./hooks/ref/UseRefMultipleCustomHooks";
 import { Contador } from "./hooks/state/Contador";
 import { ContadorConCustomHook } from "./hooks/state/ContadorConCustomHook";
 
-ReactDOM.render(<HookUseMemo />, document.getElementById("root"));
+ReactDOM.render(<TodoApp />, document.getElementById("root"));
+
+//import("./hooks/reducer/intro-reducer");
